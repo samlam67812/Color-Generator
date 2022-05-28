7e5e27e6bd1337a7483f2578d954a3378795c06e
samlam67812 <90791465+samlam67812@users.noreply.github.com>
2022-05-28 23:07:12 +0800

    Updates

---
+++ precache-manifest.2ba09cd3acfbfba8bbebb52f2e44abdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ba7ea04929ca7fb973d6b222cbd23e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d03fda9268b16a256f2",
+    "url": "/static/css/main.f6e8d1ab.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d03fda9268b16a256f2",
+    "url": "/static/js/main.e10de84c.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
